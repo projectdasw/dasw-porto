2e723c4aa8f859788514089aef4304b90f124197
projectdasw <project.dasw@gmail.com>
2021-11-21 10:51:40 +0700

    Update dasw-javascript.js

--- assets/js/dasw-javascript.js
+++ assets/js/dasw-javascript.js
@@ -2,8 +2,15 @@ window.onscroll = function () {
     myFunction()
 };
 
+document.onscroll = function () {
+    scrollFunction()
+};
+
+var mybutton = document.getElementById("btt-btn");
 var navbar = document.getElementById("nav");
 var navmenu = document.getElementById("nav-menu");
+var opennav = document.getElementById("dasw-open-bars");
+var closenav = document.getElementById("dasw-close-bars");
 var sticky = navbar.offsetTop;
 
 function myFunction() {
@@ -16,7 +23,39 @@ function myFunction() {
     }
 }
 
-function dasw_nav() {
+function dasw_opennav() {
     navmenu.classList.remove("dasw-menunav");
     navmenu.classList.add("dasw-menunav-bars");
+    closenav.style.display = "flex";
+    closenav.style.fontSize = "30px";
+    closenav.style.padding = "10px";
+    closenav.style.justifyContent = "flex-end";
+    closenav.style.animation = "dasw-closenav 1s ease 0s 1 normal forwards";
+    opennav.style.display = "none";
+}
+
+function dasw_closenav() {
+    navmenu.classList.add("dasw-menunav");
+    navmenu.classList.remove("dasw-menunav-bars");
+    closenav.style.display = "none";
+    opennav.style.display = "flex";
+    opennav.style.animation = "dasw-opennav 1s ease 0s 1 normal forwards";
+}
+
+function scrollFunction() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        mybutton.style.display = "flex";
+        mybutton.classList.add("fadeInUp");
+        mybutton.classList.remove("fadeOutDown");
+    } else {
+        mybutton.classList.remove("fadeInUp");
+        mybutton.classList.add("fadeOutDown");
+    }
+}
+
+function topFunction() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
 }
